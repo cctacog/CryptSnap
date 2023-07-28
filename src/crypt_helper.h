d6218dce9e60e30bcd850ff8e469700ee08c028a
Sophia Garcia <gasophia2@gmail.com>
2023-07-27 21:37:41 -0700

    update

--- src/crypt_helper.h
+++ src/crypt_helper.h
@@ -1,34 +1,15 @@
 #ifndef CRYPT_HELPER
 #define CRYPT_HELPER
 
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "reference.h"
+#include "the_round.h"
 #include "stack_ops.h"
+#include "queue_ops.h"
 
 /*
 Sophia Garcia
 01-31-2023
 */
-
-//this is usng a macro to define and initialize any future new instances
-enum Operator {AND, OR, NAND, NOR, XOR, XNOR, NONE} Operator;
-typedef struct The_Round
-{
-    Stack stack;
-    char code[100];
-    char key[100];
-    char secret[100];
-    int code_len;
-    int play_again;
-} The_Round;
-
-//the_round functions
-void transfer(const The_Round terms_1, The_Round *terms_2);
 
 //calculator functions
 char possibleActions(enum Operator i, char c, char k);
