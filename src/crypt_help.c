548f9ee3b6a2df9296318617177c7cc7566490ee
Sophia Garcia <gasophia2@gmail.com>
2023-08-01 12:12:35 -0700

    edited vars

--- src/crypt_help.c
+++ src/crypt_help.c
@@ -72,23 +72,22 @@ void secret_thru_queue(The_Round *terms)
 void secret_thru_stack(The_Round *terms)
 {
     int iK = 0;
-    for(int q = 0; isEmpty_s(&(terms->stack)) == 1; ++q)
+    for(int s = 0; isEmpty_s(&(terms->stack)) == 1; ++s)
     {
         enum Operator op = pop(&(terms->stack));
-        for(int i = 0; (i < terms->code_len) && (terms->code[i] != '\0'); i++)
+        for(int i = 0; (i < terms->code_len) && (terms->secret[i] != '\0'); i++)
         {
             /*
             because a character cannot xor itself the
             array location will equal the said character
             */
-            if(terms->code[i] == terms->key[iK] && (op == XOR || op == XNOR))
-            {
-                terms->secret[i] = terms->code[i];
+            if(terms->secret[i] == terms->key[iK] && (op == XOR || op == XNOR))
+            {                
                 continue;
             }
 
             char s;
-            s = possibleActions(op, terms->code[i], terms->key[iK]);
+            s = possibleActions(op, terms->secret[i], terms->key[iK]);
 
             terms->secret[i] = s;
 
@@ -97,9 +96,7 @@ void secret_thru_stack(The_Round *terms)
             then the secret will end at i+1
             */
             iK++;
-
-            if(terms->code[i+1] == '\0')
-                terms->secret[i+1] = '\0';
+            
             if(terms->key[iK] == '\0')
                 iK = 0;
         }
--- src/encrypt_decrypting.c
+++ src/encrypt_decrypting.c
@@ -16,21 +16,25 @@ The_Round crypt(const The_Round terms_)
         no leaking memorey
     */
     The_Round terms;
-    if(terms_.code[0] == '\0')
-    {        
+    char prev_use[MAX];
+    printf("Would you like to use the previous code & key? [ex: no]: ");
+    gets(prev_use);
+
+    if(prev_use[0] == 'y' || prev_use[0] == 'Y')
+    {       
+        transfer(terms_, &terms);
+        empty_q(&terms);
+        empty_s(&terms);         
+    } 
+    else
+    {
         printf("Enter code: ");
         gets(terms.code);
         terms.code_len = strlen(terms.code);
         printf("Enter key: ");
         gets(terms.key);  
         def_values_q(&terms.queue);
-        def_values_s(&terms.stack); 
-    } 
-    else
-    {
-        transfer(terms_, &terms);
-        empty_q(&terms);
-        empty_s(&terms);
+        def_values_s(&terms.stack);
     }
 
     /*
@@ -38,7 +42,7 @@ The_Round crypt(const The_Round terms_)
     from the operation of the code and key at the said array location
     */
 
-    char operate[20];
+    char operate[MAX];
     printf("\n\nWhat operation(s) would you like to use?\n1.AND\n2. OR\n3. NAND\n4. NOR\n5. XOR\n6. XNOR\nOperator(s) [ex: 5231 or 2]: ");
     gets(operate);
     enum Operator op;
@@ -93,14 +97,18 @@ The_Round crypt(const The_Round terms_)
         else printf("%c", alphabet(num));
     }
 
-    char replay [10];
+    char replay[MAX];
 
     printf("\n\nWould you care to play again?: [ex: yes]  ");
     gets(replay);
 
-    if(replay[0] == 'y')
+    if(replay[0] == 'y' || replay[0] == 'Y')
     {
         terms.play_again = 1;       
+    }
+    else
+    {
+        terms.play_again = 0;
     }
 
     // free(code);
