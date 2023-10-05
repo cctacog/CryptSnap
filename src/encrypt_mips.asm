3cdacdfe0a68cc6a615e692bc565c8e09bd1071d
Sophia Garcia <gasophia2@gmail.com>
2023-10-04 20:36:29 -0700

    deleted files because not using vscode as IDE or running interface

--- src/encrypt_mips.asm
+++ src/encrypt_mips.asm
@@ -7,7 +7,7 @@
 	prompt: 	.asciiz 	"Solve integers 1000 and 1010 through the gate "
 	boo_:	.asciiz	"Boo fail\n"
 	yay_: 	.asciiz	"Yay you got it\n!"
-	res: 	.space	6 	#for three digit num plus a null pointer end
+	res: 	.space	5	#for four digit num plus a null pointer end
 	_and: 	.asciiz 	"AND:"
 	_or: 	.asciiz 	"OR:"
 	_nand:	.asciiz 	"NAND:"
@@ -36,6 +36,7 @@
 		li	$v0, 4
 		la	$a0, boo_
 		syscall
+		lw	$t7, 1
 		jr 	$ra
 	yay:	#user success
 		li	$v0, 4
@@ -44,25 +45,25 @@
 		jr	$ra
 
 	for_translation:	#translated users char input to an array of integers
-		move	$t6, $a1
+		lb	$t6, 0($a1)
 		andi	$t6, $t6, 0xF
-		move	$a0, $t6
-		addi	$a1, $a1, 4
-		addi	$a0, $a0, 4
+		sw	$t6, res_arr($t7)
+		addi	$a1, $a1, 1
 		addi	$a3, $a3, 1
 		bne 	$a3, 4, for_translation
-		lw	$v0, -12($a0)
 		jr 	$ra
 
-	compare_:	#comapres user input with actual calculated result
+	compare_:	#compares user input with actual calculated result
 		lb	$t5, 0($a0)
-		lb	$t6, 0($a1)
-		seq	$t7, $t5, $t6
-		bne 	$t7, 1, boo
-		addi	$a0, $a0, 4
-		addi	$a1, $a1, 4
-		addi	$a3, $a3, 1
-		bne 	$a3, 4, compare_
+		srlv	$t6, $a1, $a2	
+		andi	$t6, $t6, 0x01
+		seq 	$t7, $t6, $t5
+		addi	$a0, $a0, 1
+		addi	$a1, $a1, 1
+		addi	$a2, $a2, 1
+		addi	$v0, $t7, 0x00
+		bne 	$v0, 1, compare_
+		blt	$a2, 4, compare_
 		jr 	$ra
 		
 	
@@ -72,10 +73,16 @@
 		syscall	
 		
 		mul 	$t1, $s3, 4  	# (tmp = n*4)	      	
-		lw 	$t1, switch($t1)   	# to be edited not a huge fan of this system fr like bleh
+		lw 	$t1, switch($t1)   	# shit i submited that note HAJAJA
 		jr	$t1
 		
 	#the following cases translate the code and the key through a specific logic gate (AND to XNOR)	
+	_AND:
+		li 	$v0, 4
+		la 	$a0, _and
+		syscall
+		and 	$s2, $s0, $s1
+		j 	gate_loop2
 	_OR:
 		li 	$v0, 4
 		la 	$a0, _or
@@ -109,13 +116,7 @@
 		not 	$s2, $t0
 		j 	gate_loop2
 	NONE:
-		j 	end	# this one ends the code/level
-	_AND:
-		li 	$v0, 4
-		la 	$a0, _and
-		syscall
-		and 	$s2, $s0, $s1
-		j 	gate_loop2
+		j 	end	# this one ends the code/level	
 	
 	gate_loop2:
 		li 	$v0, 8
@@ -123,15 +124,19 @@
 		li 	$a1, 5
 		syscall
 		
-		lw 	$a0, res_arr
-		lw	$a1, res
+		la	$a1, res
 		li	$a3, 0
-		jal 	for_translation	#goes through gate per the array (AND = 6 then NONE = 0)
-		sw	$v0, res_arr
-		lw 	$a0, res_arr
-		move	$a1, $s2
+		jal 	for_translation	# goes through gate per the array (AND = 6 then NONE = 0)			
+		
+		addi	$t7, $t7, 0x00
+		la 	$a0, res_arr
+		addi	$a1, $s2, 0x00
 		li	$a2, 0
 		jal	compare_
+		
+		beq	$v0, 1, yay
+		jal	boo		
+		
 		addi	$s3, $s3, -1
 		j	gate_loop1
 		
